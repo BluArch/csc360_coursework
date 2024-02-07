426ee91c9f5dcdfe8caadc077d90a995dba0be72
jcruikshanks <jcruikshanks@uvic.ca>
2024-02-07 23:36:13 +0000

    Commenting finished

--- assign1/.ipynb_checkpoints/fetch-info-checkpoint.c
+++ assign1/.ipynb_checkpoints/fetch-info-checkpoint.c
@@ -26,6 +26,10 @@
  */
 
 void print_cpu_info(){
+     /*
+    Function opens the /proc/cpu directory and prints the line containing the model name and the line containing
+    the number of cpu cores.
+    */
     FILE *input = fopen("/proc/cpuinfo", "r");
     char line[200];
 
@@ -41,6 +45,10 @@ void print_cpu_info(){
 }
 
 void print_linux_version(){
+    /*
+    Function opens the /proc/version directory and prints the line containing the current version of Linux 
+    of the system.
+    */
     FILE *input = fopen("/proc/version", "r");
     char line[200];
 
@@ -51,6 +59,9 @@ void print_linux_version(){
 }
 
 void print_memory_info(){
+    /*
+    Function opens the /proc/version directory and prints the line the total amount of memory of the system.
+    */
     FILE *input = fopen("/proc/meminfo", "r");
     char line[200];
 
@@ -63,6 +74,10 @@ void print_memory_info(){
 }
 
 void print_uptime(){
+    /*
+    Function opens the /proc/uptime directory and converts the number of uptime seconds into days, hours,
+    minutes, and seconds. The values are then printed
+    */
     FILE *input = fopen("/proc/uptime", "r");
     char line[200];
     int uptime;
@@ -90,30 +105,37 @@ void print_uptime(){
 
 
 void print_process_info(char * process_num) {
+    /*
+    Function take the process_num as input. It opens the /proc/process_num/status directory and prints the 
+    name of the process, the console command that started the process if there is one, the number of threads 
+    running in the process, and the total number of context switches that have occurred during the running of 
+    the process.
+    */
     int process = atoi(process_num);
     char path_name[30];
-    sprintf(path_name, "/proc/%d/status", process);
+    sprintf(path_name, "/proc/%d/status", process);      // Add the process number to the filepath name
     FILE *input = fopen(path_name, "r");
+    
     if (input!=NULL){
         char line[200];
         int total_switches = 0;
         fprintf(stdout, "Process number: %d\n", process);
         while (fgets(line, sizeof(line), input)!=NULL){
             if (strstr(line, "Name:")!=NULL){
-                fprintf(stdout, "%s", line); 
+                fprintf(stdout, "%s", line);              // Print the name of the process
                 char *token = strtok(line, ":");
                 token = strtok(NULL, ":");
-                fprintf(stdout, "Filename (if any):%s", token);
+                fprintf(stdout, "Filename (if any):%s", token);    // After tokenizing, add to filename string and print 
             } else if (strstr(line, "Threads:")!=NULL){
-                fprintf(stdout, "%s", line);
+                fprintf(stdout, "%s", line);                       // Print the number of threads of the procecss
             } else if (strstr(line, "voluntary_ctxt_switches:")!=NULL || strstr(line, "nonvoluntary_ctxt_switches:")!=NULL){
                 char *token = strtok(line, ":");
                 token = strtok(NULL, ":");
-                total_switches += atoi(token);
+                total_switches += atoi(token);                    // Add the number of voluntary context switches and nonvoluntary context switches
             }
         }
-    fprintf(stdout, "Total context switches: %d\n", total_switches);
-    fclose(input);
+    fprintf(stdout, "Total context switches: %d\n", total_switches);     // Print total number of switches
+    fclose(input);  
     } else {
         fprintf(stdout, "Process number %d not found.\n", process);
     }
@@ -121,6 +143,11 @@ void print_process_info(char * process_num) {
 
 
 void print_full_info() {
+    /*
+    Function calls helper functions to print the model name of the CPU, the number of cores, the version of Linux running 
+    in the environment, the total memory available to the system, and the amount of time the system has been running in terms 
+    of days, hours, minutes and seconds.
+    */
     print_cpu_info();
     print_linux_version();
     print_memory_info();
@@ -129,6 +156,9 @@ void print_full_info() {
 
 
 int main(int argc, char ** argv) {  
+    /*
+    Main function chooses with type of output to send based on the argument from the command line.
+    */
     if (argc == 1) {
         print_full_info();
     } else {
--- assign1/.ipynb_checkpoints/pipe4-checkpoint.c
+++ assign1/.ipynb_checkpoints/pipe4-checkpoint.c
@@ -25,33 +25,40 @@
 #define MAX_NUM_INPUTS 4
 
 void tokenize_input(char input[MAX_LINE_LEN], char commands[MAX_NUM_INPUTS][MAX_NUM_TOKEN][MAX_LINE_LEN], int row, int num_arguments[4]){
+    /*
+    Function takes a one dimensional character array, a 2D array, an integer, and an array of integers as input.
+    The string in the array "input" is tokenized with the contents being stored in the 2D at the given row index
+    The number of arguments the current row contains is stored in num_arguments.
+    */
     char *token;
     token = strtok(input, " ");
-    int index = 0;
     int arguments = 0;
 
     while(token!=NULL){
-        strcpy(commands[row][arguments], token);
+        strcpy(commands[row][arguments], token);     // Store current token value into commands
         arguments++;
-        index++;
         token = strtok(NULL, " ");
     }
-    num_arguments[row] = arguments;
-    strcpy(commands[row][index], "\0");
+
+    num_arguments[row] = arguments;     // Number of arguments are stored to be refered to later
+    strcpy(commands[row][arguments], "\0");
 }
 
-int get_command_inputs(char commands[MAX_NUM_INPUTS][MAX_NUM_TOKEN][MAX_LINE_LEN], char input[MAX_LINE_LEN], int num_arguments[4]){
+int get_command_inputs(char commands[MAX_NUM_INPUTS][MAX_NUM_TOKEN][MAX_LINE_LEN], int num_arguments[4]){
     /*
-    Function takes input from the command line for commands and arguments, store given input into a given 3D array base on where 
+    Function takes a 2D array, a one dimensional character array, and an array of integers as input.
+    Input is taken from the command line using read and sent to tokenize_input to be parsed into "commands".
+    The number of rows read from the command line is then returned.
     */
+    char input[MAX_LINE_LEN];
     int num_row = 0;
 
     for (int i = 0; i<MAX_NUM_INPUTS; i++){
-        int num_chars_read = read(0, input, MAX_LINE_LEN);
+        int num_chars_read = read(0, input, MAX_LINE_LEN);       // Take input from command line
         if (num_chars_read==1){
             break;
         } else {
-            input[num_chars_read-1] = '\0';
+            input[num_chars_read-1] = '\0';      // Replace newline character with terminating 0
             tokenize_input(input, commands, num_row, num_arguments);
             num_row++;
         }
@@ -60,12 +67,17 @@ int get_command_inputs(char commands[MAX_NUM_INPUTS][MAX_NUM_TOKEN][MAX_LINE_LEN
 }
 
 int main() {
+    /*
+    Main function sends the 2D array "commands" to be filled by reading inputs from the command line. The number of lines of input is stored in "num_rows"
+    with the number of arguments per row stored in "num_arguments". The command and parameters of each row is piped from one child to the next using the 
+    file descriptors "fd_old" and "fd_new". Each iteration of the loop, the new file descriptor is swapped to become the old one. The function results in the piped commands
+    being executed.
+    */
     char commands[MAX_NUM_INPUTS][MAX_NUM_TOKEN][MAX_LINE_LEN];
-    char input[MAX_LINE_LEN];
-    int num_arguments[MAX_NUM_INPUTS] = {0};
     int num_rows;
+    int num_arguments[MAX_NUM_INPUTS] = {0};
     
-    num_rows = get_command_inputs(commands, input, num_arguments);
+    num_rows = get_command_inputs(commands, num_arguments);
     
     char *envp[] = { 0 };
     int status;
@@ -77,38 +89,40 @@ int main() {
         pipe(fd_new);
         char *cur_commands[MAX_NUM_TOKEN];
 
-        // Create pointer array for curr_command line to execute
-        for (int j = 0; j<num_arguments[i]; j++){
+        
+        for (int j = 0; j<num_arguments[i]; j++){    // Move current row contents of commands into cur_commands
             cur_commands[j] = commands[i][j]; 
         }
         cur_commands[num_arguments[i]] = NULL;
         
 
         if((pid = fork())==0){
-            if (i!=0){
+            if (i!=0){                  // On all iterations except the first, we redirect stdIn from the previous file descriptor
                 dup2(fd_old[0], 0);
                 close(fd_old[1]);
                 close(fd_old[0]);
             }
 
-            if(i!=(num_rows-1)){
-                dup2(fd_new[1], 1);
+            if(i!=(num_rows-1)){        // On all iterations except the last, re redirect stdOut to the new file descriptor
+                dup2(fd_new[1], 1);     
                 close(fd_new[1]);
                 close(fd_new[0]);
             }
             execve(cur_commands[0], cur_commands, envp);         
         }
         
-        if(i!=0){
+        if(i!=0){                      // On all iterations except the first, we close the old file descriptor
             close(fd_old[1]);
             close(fd_old[0]);
         }
-        fd_old[0] = fd_new[0];
-        fd_old[1] = fd_new[1];
+        if(i!=(num_rows-1)){           // On all iterations except the last, the "new" file descriptor becomes the "old" file descriptor
+            fd_old[0] = fd_new[0];         
+            fd_old[1] = fd_new[1];
+        }
     }
-    close(fd_new[1]);
+    close(fd_new[1]);                  // Close the new file descriptor
     close(fd_new[0]);
     
-    while(wait(&status)>0);
+    while(wait(&status)>0);            // Wait until the children have all finished
     exit(0);
 }
